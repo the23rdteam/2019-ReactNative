1f2709f023da7333ee1dc1f626427c130d27bdf5
Chang Doh <cwdoh@riotgames.com>
2019-06-02 23:38:25 +0900

    11. width

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ class CounterButton extends React.Component {
 
   render() {
     return (
-      <View>
+      <View style={styles.buttonContainer}>
         <Text>{this.state.counter}</Text>
         <Button title={'click me!'} onPress={this.clickHandler}/>
       </View>
@@ -49,5 +49,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  buttonContainer: {
+    width: 300,
   },
 });
