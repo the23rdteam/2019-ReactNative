56041500a6ba3c385cc504399d70342034427da9
Chang Doh <cwdoh@riotgames.com>
2019-06-03 01:23:44 +0900

    33. Add detail screen

---
+++ WeatherDetailScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Constants } from 'expo';
+
+export default class WeatherDetailScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Weather Information',
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginTop: Constants.statusBarHeight,
+  },
+});
