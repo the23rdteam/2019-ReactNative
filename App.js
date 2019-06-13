808058eb7927fbc78eaedf1691158ec05f82eceb
IMHOJEONG <39ghwjd@naver.com>
2019-06-14 00:40:07 +0900

    Add MainPage component

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import React, {Component} from 'react';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import CityList from './CityList';
 import WeatherDetailScreen from './WeatherDetailScreen';
+import MainPage from './MainPage'
 
 const AppNavigator = createStackNavigator(
   {
+    MainPage : MainPage,
     CityList: CityList,
     Detail: WeatherDetailScreen,
   },
   {
-    initialRouteName: 'CityList'
+    initialRouteName: 'MainPage'
   }
 );
 
---
+++ MainPage.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import { FlatList, StyleSheet, Text, TouchableOpacity, ScrollView} from 'react-native';
+import { Constants } from 'expo';
+import { Card, CardTitle, CardContent, CardAction, CardButton, CardImage } from 'react-native-cards';
+
+
+
+export default class MainPage extends React.Component {
+  static navigationOptions = {
+    title: 'Don`t you go This places??',
+    headerStyle:{
+      backgroundColor: '#f4511e',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+  };
+
+  constructor(props) {
+    super(props);
+
+  }
+
+//   componentDidMount() {
+//     fetch('http://demo6468405.mockable.io/weather-crawlers/cities')
+//       .then(response => response.json())
+//       .then(cities => {
+//         console.log('cities =', cities.length);
+//         this.setState({
+//           cities
+//         });
+//       });
+//   }
+
+//   onPressCity(item) {
+//     this.props.navigation.navigate(
+//       'Detail',
+//       {
+//         city: item
+//       }
+//     );
+//   }
+
+  render() {
+    return (
+      <> 
+        <ScrollView>
+        <Card style={styles.container}>
+            <CardImage 
+              source={{uri: 'http://bit.ly/2GfzooV'}} 
+              title="Clifton, Western Cape"
+            />
+            <CardTitle
+              subtitle="Beautiful Sunset!"
+            />
+            <CardContent text="Cape Town Clifton, South Africa" />
+        </Card>
+        <Card style={styles.container}>
+            <CardImage 
+              source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/1/1d/Daejeon_montage.png'}} 
+              title="Daejeon, Korea"
+            />
+            <CardTitle
+              subtitle="Technical City of Korea"
+            />
+            <CardContent text="Daejeon, Korea" />
+
+        </Card>
+        <Card style={styles.container}>
+            <CardImage 
+              source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Seoul_City_Montage.png/375px-Seoul_City_Montage.png'}} 
+              title="Seoul, Korea"
+            />
+            <CardTitle
+              subtitle="Capital of Korea"
+            />
+            <CardContent text="Seoul, Korea" />
+            
+        </Card>
+     
+    </ScrollView>
+        <TouchableOpacity style = {styles.button} onPress={() => this.props.navigation.navigate('CityList')}>
+            <Text style={styles.text}>Check Weather!</Text>
+        </TouchableOpacity>
+      </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginTop: Constants.statusBarHeight,
+  },
+
+  button: {
+    flex: 1,
+    height: "50%",
+    justifyContent: 'center',
+    borderRadius: 50,
+    marginTop:50,
+    marginBottom:10,
+    borderWidth: 1,
+    borderColor: 'orange',
+    color: 'rgba(0, 255, 255, 0.7)',
+  },
+  text: {
+    fontSize: 30,
+    textAlign: 'center',
+
+  }
+});
+
